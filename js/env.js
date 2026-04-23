10a88cadfdb8cc5b722037abc8246660b51ea6ed
Abihaniisa <bayeroisa2003@gmail.com>
2026-04-23 22:31:11 +0100

    Add environment configuration for Supabase and WhatsApp

---
+++ js/env.js
@@ -0,0 +1,5 @@
+const ENV = {
+    SUPABASE_URL: 'https://hibpuvurlvkuqjawkqlu.supabase.co',
+    SUPABASE_ANON_KEY: 'sb_publishable_Mb6I2-X_6J2Hl-MbcAQRzA_mgWsduoA',
+    WHATSAPP_NUMBER: '+2347067551684'
+};
