97e2bb7c842cd251c7007a6146c6fda2a98c24d9
Abihaniisa <bayeroisa2003@gmail.com>
2026-04-24 06:37:34 +0100

    Fix syntax error in env.js

--- js/env.js
+++ js/env.js
@@ -1,3 +1,4 @@
+// ABIHANI EXPRESS - Environment Configuration
 const ENV = {
     SUPABASE_URL: 'https://hibpuvurlvkuqjawkqlu.supabase.co',
     SUPABASE_ANON_KEY: 'sb_publishable_Mb6I2-X_6J2Hl-MbcAQRzA_mgWsduoA',
